4b43e7bb2a6dfe5e9428c97fc20c148efa651b1f
ahmad abdulazeez <ahmadabdulazeez354@gmail.com>
2020-09-29 13:45:20 +0100

    feat: exercise1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Header from './component/header'
-import Content from './component/content'
-import Total from './component/total'
 
 const App = () => {
   const course = 'Half Stack application development'
@@ -15,15 +12,19 @@ const App = () => {
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={part1} exercises={exercises1} />
-      <Content parts={part2} exercises={exercises2} />
-      <Content parts={part3} exercises={exercises3} />
-      <Total total={exercises1 + exercises2 + exercises3} />
+      <h1>{course}</h1>
+      <p>
+        {part1} {exercises1}
+      </p>
+      <p>
+        {part2} {exercises2}
+      </p>
+      <p>
+        {part3} {exercises3}
+      </p>
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
     </div>
   )
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
-
-export default App;
