a46a517acd5003fa9a2c162922d3c4e4410c57f6
ahmad abdulazeez <ahmadabdulazeez354@gmail.com>
2020-10-13 12:06:26 +0100

    feat: exercise2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react'
-
+import React, { useState, useEffect} from 'react'
+import Axios from 'axios'
 import Search from './component/Search'
 
 import Phonebook from './component/Phonebook'
@@ -11,16 +11,17 @@ const generateId = () => Math.random().toString(36).substring(2, 6)
 
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { id:1, name: 'Arto Hellas', number: '040-123456' },
-    { id:2, name: 'Ada Lovelace', number: '39-44-5323523' },
-    { id:3, name: 'Dan Abramov', number: '12-43-234345' },
-    { id:4, name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [query, setQuery] = useState('')
 
+  useEffect(() => {
+   Axios.get('http://localhost:3030/persons')
+        .then(response => setPersons(response.data))
+        .catch(console.log)
+   
+  },[])
   const addNewName = (e) => {
     e.preventDefault();
 
